dc7c37ac7ab6d301a65a6aa9899ec1ef76dc1e04
buivanthien <57850451+buivanthien1@users.noreply.github.com>
2021-03-17 11:32:08 +0700

    Update NGROK-CHECK.bat

--- NGROK-CHECK.bat
+++ NGROK-CHECK.bat
@@ -19,7 +19,7 @@ sc config Audiosrv start= auto >nul
 sc start audiosrv >nul
 ICACLS C:\Windows\Temp /grant administrator:F >nul
 ICACLS C:\Windows\installer /grant administrator:F >nul
-echo Hoàn tất! Kết nối VPS của bạn bằng RDP. Khi RDP hết hạn VPS sẽ tắt, hãy chạy lại các công việc để nhận RDP mới.
+echo VPS đã đc tạo! Kết nối VPS của bạn bằng Remote Desktop (RDP).
 echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Không thể tải đường NGROK tunnel, vui lòng dán NGROK TOKEN mới vào YML. Kiểm tra tunnel tại đây: https://dashboard.ngrok.com/status/tunnels " && exit
 echo User: administrator
